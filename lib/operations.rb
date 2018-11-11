736dd22cf8b3eda350e1df192a266c0ec076cda4
Britny Edwards <bredwards2@gmail.com>
2018-11-11 00:19:38 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+ if speed < 40 || speed > 60
+   puts "true"
+   unless speed == 50
+   else 
+   puts "false"
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 && speed > 60 ? "true" : "false"
 end
-	
+end
 
 
